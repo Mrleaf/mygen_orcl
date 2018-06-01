7d6ded1120f0f1cfcb956f80b90c240289669c05
leaf <leaf_90@qq.com>
2018-06-01 14:50:16 +0800

    配置

--- src/main/java/generator/Test.java
+++ src/main/java/generator/Test.java
@@ -1,34 +1,42 @@
 package generator;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Test {
-	public static void main(String[] args) {
-		String str = "T_TEST";
+    public static void main(String[] args) {
+        String str ="SYS_USER,KEY_ENTITY_INFO";
+        toTableXml(str);
+        toDataConfigXml(str);
+        System.out.println("-------");
+        managedSchema(str);
+        str = "t_lic_equipment_producted";
         System.out.println(toCamelCase(str));
         System.out.println(toCapitalizeCamelCase(str));
-        
-    
- 	}
-	
-	private static final char SEPARATOR = '_';
-	 
+        System.out.println(toUnderlineName(str));
+
+    }
+
+    private static final char SEPARATOR = '_';
+
     public static String toUnderlineName(String s) {
         if (s == null) {
             return null;
         }
- 
+
         StringBuilder sb = new StringBuilder();
         boolean upperCase = false;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
- 
+
             boolean nextUpperCase = true;
- 
+
             if (i < (s.length() - 1)) {
                 nextUpperCase = Character.isUpperCase(s.charAt(i + 1));
             };
- 
+
             if ((i >= 0) && Character.isUpperCase(c)) {
                 if (!upperCase || !nextUpperCase) {
                     if (i > 0) sb.append(SEPARATOR);
@@ -37,25 +45,25 @@ public class Test {
             } else {
                 upperCase = false;
             }
- 
+
             sb.append(Character.toLowerCase(c));
         }
- 
+
         return sb.toString();
     }
- 
+
     public static String toCamelCase(String s) {
         if (s == null) {
             return null;
         }
- 
+
         s = s.toLowerCase();
- 
+
         StringBuilder sb = new StringBuilder(s.length());
         boolean upperCase = false;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
- 
+
             if (c == SEPARATOR) {
                 upperCase = true;
             } else if (upperCase) {
@@ -65,10 +73,87 @@ public class Test {
                 sb.append(c);
             }
         }
- 
+
         return sb.toString();
     }
- 
+
+    public static List<String> str2List(String str, String regex){
+        if(str==null||str=="")
+            return null;
+        str = str.replaceAll(" ","").replaceAll("\t","").replaceAll("\n","");
+        String[] arrayStr = str.split(regex);
+        List<String> list = Arrays.asList(arrayStr);
+        List arrayList = new ArrayList(list);
+        return arrayList;
+    }
+
+    public static void toTableXml(String str) {
+        List<String> list = str2List(str,",");
+        for(String n:list){
+            String s = n.toLowerCase();
+            StringBuilder sb = new StringBuilder();
+            boolean upperCase = false;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == SEPARATOR) {
+                    upperCase = true;
+                } else if (i==0 ||upperCase) {
+                    sb.append(Character.toUpperCase(c));
+                    upperCase = false;
+                } else {
+                    sb.append(c);
+                }
+            }
+            System.out.println("<table  tableName=\""+n+"\"   domainObjectName=\""+sb.toString()+"\"/>");
+        }
+
+    }
+
+    public static void toDataConfigXml(String str) {
+        List<String> list = str2List(str,",");
+        for(String n:list){
+            String s = n.toLowerCase();
+            StringBuilder sb = new StringBuilder();
+            boolean upperCase = false;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == SEPARATOR) {
+                    upperCase = true;
+                } else if (i==0 ||upperCase) {
+                    sb.append(Character.toUpperCase(c));
+                    upperCase = false;
+                } else {
+                    sb.append(c);
+                }
+            }
+            System.out.println("<field column=\""+n+"\"   name=\""+sb.substring(0, 1).toLowerCase() + sb.substring(1)+"\"/>");
+        }
+
+    }
+
+    public static void managedSchema(String str) {
+        List<String> list = str2List(str,",");
+        for(String n:list){
+            String s = n.toLowerCase();
+            StringBuilder sb = new StringBuilder();
+            boolean upperCase = false;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == SEPARATOR) {
+                    upperCase = true;
+                } else if (i==0 ||upperCase) {
+                    sb.append(Character.toUpperCase(c));
+                    upperCase = false;
+                } else {
+                    sb.append(c);
+                }
+            }
+            System.out.println("<field name=\""+sb.substring(0, 1).toLowerCase() + sb.substring(1)+"\" " +
+                    "type=\"string\"  indexed=\"true\" stored=\"false\" " +
+                    "/>");
+        }
+
+    }
     public static String toCapitalizeCamelCase(String s) {
         if (s == null) {
             return null;
